41b874abf25fc32ba53783ffd97dd816d6d23404
GhuniNew <122639635+ghuninew1@users.noreply.github.com>
2024-06-18 12:11:28 +0700

    update News

--- src/components/pages/news/NewsIframe.jsx
+++ src/components/pages/news/NewsIframe.jsx
@@ -1,42 +1,46 @@
-import { XCircleIcon } from "@heroicons/react/24/outline";
+// import { XCircleIcon } from "@heroicons/react/24/outline";
 import PropTypes from "prop-types";
 
 const NewsIframe = ({ src, handlClose }) => {
-    return (
-        <div
-            className="fixed w-full min-h-screen bg-black top-0 left-0 z-50 animate-fade"
-            onClick={handlClose}
-        >
-            <section className="relative w-full h-full mt-1 md:mt-10 px-2">
-                <div className="absolute top-0 w-full left-0 flex items-center justify-center mx-auto min-h-screen ">
-                    {src && (
-                        <>
-                            <iframe
-                                src={src}
-                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                                allowFullScreen
-                                loading="lazy"
-                                frameBorder="0"
-                                onClick={(e) =>
-                                    window.open(e.target.src, "_blank")
-                                }
-                                width={"100%"}
-                                height={"auto"}
-                                className="block h-full md:min-h-[720px] min-h-[480px] max-w-[1100px]"
-                            />
-                        </>
-                    )}
-                </div>
+    return ( src && <>
+            {window.open(src, "_blank")}
+            {handlClose()}
+        </>)
+        // <div
+        //     className="fixed w-full min-h-screen bg-black top-0 left-0 z-50 animate-fade"
+        //     onClick={handlClose}
+        // >
+        //     <section className="relative w-full h-full mt-1 md:mt-10 px-2">
+        //         <div className="absolute top-0 w-full left-0 flex items-center justify-center mx-auto min-h-screen ">
+        //             {src && (
+        //                 <>
+        //                 {window.open(src, "_blank")}
+        //                     {/* <iframe
+        //                         src={src}
+        //                         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        //                         allowFullScreen
+        //                         loading="lazy"
+        //                         frameBorder="0"
+        //                         onClick={(e) =>
+        //                             window.open(e.target.src, "_blank")
+        //                         }
+        //                         width={"100%"}
+        //                         height={"auto"}
+        //                         className="block h-full md:min-h-[720px] min-h-[480px] max-w-[1100px]"
+        //                     /> */}
+        //                 </>
+        //             )}
+        //         </div>
 
-                <button className="opacity-50 hover:opacity-100 absolute top-0 right-2 z-[999] inline-flex rounded-full ransition-opacity ">
-                    <XCircleIcon
-                        className="hover:scale-110 transform transition-all hover:text-red-600 text-[#a74747] w-[50px] h-[50px]"
-                        onClick={handlClose}
-                    />
-                </button>
-            </section>
-        </div>
-    );
+        //         <button className="opacity-50 hover:opacity-100 absolute top-0 right-2 z-[999] inline-flex rounded-full ransition-opacity ">
+        //             <XCircleIcon
+        //                 className="hover:scale-110 transform transition-all hover:text-red-600 text-[#a74747] w-[50px] h-[50px]"
+        //                 onClick={handlClose}
+        //             />
+        //         </button>
+        //     </section>
+        // </div>
+    // );
 };
 
 NewsIframe.propTypes = {

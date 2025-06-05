da1ce0f981c4f0e1e4e429d8d106b7815cf09044
GhuniNew <aakanun43@gmail.com>
2025-06-05 15:20:29 +0700

    update card ppang ptum pnote ontop

--- src/components/pages/cardPtum.jsx
+++ src/components/pages/cardPtum.jsx
@@ -1,4 +1,5 @@
 import ptum from "../../assets/ptum.png";
+import onTop from "../../assets/Tum_note_pang.png";
 import { Title } from "..";
 
 const Card = () => {
@@ -7,8 +8,9 @@ const Card = () => {
             <Title title="Card P Tum" />
             <div className="flex items-center justify-center max-w-screen-2xl mx-auto min-h-screen">
 
-                    <div className="flex items-center justify-center h-full">
+                    <div className="flex items-center justify-center h-full md:flex-row flex-col">
                         <img src={ptum} alt="ptum" loading="lazy" className="rounded-md w-full h-full" />
+                        <img src={onTop} alt="onTop" loading="lazy" className="rounded-md w-full h-full" />
                     </div>
             </div>
         </>

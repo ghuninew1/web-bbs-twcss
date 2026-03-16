6eebaf09b86eeaede39afec3c6e058b8e0623df4
GhuniNew <aakanun43@gmail.com>
2026-03-16 16:30:01 +0700

    Update Card Ptood

--- src/components/pages/cardPtood.jsx
+++ src/components/pages/cardPtood.jsx
@@ -5,7 +5,7 @@ import { Title } from "..";
 const Card = () => {
     return (
         <>
-            <Title title="Card P Pang" />
+            <Title title="Card P Tood" />
             <div className="flex items-center justify-center max-w-screen-2xl mx-auto min-h-screen">
 
                     <div className="flex items-center justify-center h-full md:flex-row flex-col">

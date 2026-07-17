3bc5390989d79b940678bc3feaa5f63284f228bc
GhuniNew <aakanun43@gmail.com>
2026-07-18 00:09:28 +0700

    update animation

--- src/components/pages/ourwork/Ourwork.jsx
+++ src/components/pages/ourwork/Ourwork.jsx
@@ -23,14 +23,13 @@ const Ourwork = () => {
     return (
         <div className=" min-h-[60vh] px-4 mb-10 max-w-[1920px] mx-auto flex flex-col items-center justify-center">
             <Title title="Our Work" />
-            <div className="grid grid-cols-2 lg:grid-cols-5 gap-12 mt-10 animate-fadeInUp">
+            <div className="grid grid-cols-2 lg:grid-cols-5 gap-12 mt-10">
                 {workData.map((work) => (
                     <div key={work.id} >
                         <Link to={work.link} 
-                        onClick={scrollToEven}
-                        className="animate-fadeInRight">
+                        onClick={scrollToEven}>
                             <img
-                                className="h-auto max-w-full max-h-[300px] rounded-2xl flex items-center justify-center hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer hover:shadow-2xl hover:shadow-gray-400 shadow-md shadow-gray-600 ring-4 ring-transparent hover:ring-[#F2F2F2] hover:ring-opacity-50"
+                                className="h-auto max-w-full max-h-[300px] rounded-2xl flex items-center justify-center hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer hover:shadow-2xl hover:shadow-gray-400 shadow-md shadow-gray-600 ring-4 ring-transparent hover:ring-[#F2F2F2] hover:ring-opacity-50 animate-fadeInUp"
                                 src={work.img}
                                 alt={work.title}
                                 loading="lazy"
